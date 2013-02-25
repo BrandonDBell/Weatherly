cf3896a780f9a3221d9566da8814897ce6fe5f11
Brandon Bell <brandondavidbell7@gmail.com>
2013-02-24 20:10:33 -0800

    commit three
    added paperclip for avatars. looks like its working fine

--- Gemfile
+++ Gemfile
@@ -22,14 +22,14 @@ end
 
 gem 'jquery-rails'
 gem 'devise'
-gem 'paperclip'
+gem 'paperclip', "~> 3.0"
 gem 'better_errors'
 gem "therubyracer"
 gem "less-rails"
 gem "twitter-bootstrap-rails"
 
 # To use ActiveModel has_secure_password
- gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar
+   has_attached_file :avatar, :styles => { :thumb => "100x100>" }, :url  => "/assets/photos/:id/:style/:basename.:extension",
+                    											   :path => ":rails_root/public/assets/photos/:id/:style/:basename.:extension"
+  validates_attachment_presence :avatar
+  validates_attachment_size :avatar, :less_than => 3.megabytes
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
+  attr_protected :avatar_file_name, :avatar_content_type, :avatar_size
   # attr_accessible :title, :body
 end
