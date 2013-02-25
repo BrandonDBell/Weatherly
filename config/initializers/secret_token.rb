3988dab1a6261dd58c647869d5d3226b73e4cc69
Brandon Bell <brandondavidbell7@gmail.com>
2013-02-24 17:50:21 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weatherly::Application.config.secret_token = '7ab7334e349610b13303e743f575e02c966add84537c51da2fa916968758fc33c313d51e6b7126f6514f6a5774b9e81b40a8a97e4d1c6f30b51ccc4612476fff'
